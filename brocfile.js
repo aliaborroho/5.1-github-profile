1b0693657625f100b4c4b00aea00efa30978a76e
Alia Borroho <aliaborroho@gmail.com>
2015-07-27 15:38:08 -0500

    basic templates/ boiler/scss

---
+++ brocfile.js
@@ -0,0 +1,22 @@
+var merge = require('broccoli-merge-trees');
+var Sass = require('broccoli-sass');
+var funnel = require('broccoli-funnel');
+var injectLivereload = require('broccoli-inject-livereload');
+
+var publicWithReload = injectLivereload('public');
+
+var includePaths = [
+  'assets/scss',
+  'bower_components/reset-css',
+  'bower_components/fontawesome/scss',
+  'bower_components/bourbon/app/assets/stylesheets',
+  'bower_components/neat/app/assets/stylesheets'
+];
+
+var vendor = funnel('bower_components', {
+  files:['handlebars/handlebars.js', 'jquery/dist/jquery.min.js']
+});
+
+var compiledCss = sass (includePaths, 'accordion.scss', 'accordion.css');
+
+module.exports = merge([publicWithReload, 'assets/js', compiledCss, vendor]);
